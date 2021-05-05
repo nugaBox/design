ed1ae194ed63737f32a18860851496c07ecf4a31
NUGA <ngjang@kakao.com>
2021-05-06 01:06:33 +0900

    부가서비스 소스정리

--- home/js/script.js
+++ home/js/script.js
@@ -10,4 +10,37 @@ $(document).ready(function() {
     $(document).on('click', 'a[href="#"]', function(e){
         e.preventDefault();
     });
+
+    // 작업 진행도 바
+    var taskNum = $('li').length;
+    var checkNum = $('.fa-check-circle').length;
+    var progress = checkNum / taskNum * 100
+    $('#progressNum').text(progress.toFixed(1)+'%');
+    $('#progressBar').progressbar({ value: progress });
+    
+    // 사이트맵 메뉴 숨김 버튼
+    var btn_hide1_click=0;
+    var btn_hide2_click=0;
+    $('.btn_hide1').on('click',function(){
+        $('.sitemap > li > ul > li > ul').slideToggle(200);		
+        btn_hide1_click++;
+        if(btn_hide1_click % 2 == 0){
+            $(this).removeClass('hide');
+            $(this).html('2차메뉴 <i class="fas fa-eye">');
+        } else {
+            $(this).addClass('hide');
+            $(this).html('2차메뉴 <i class="fas fa-eye-slash">');
+        }
+    });
+    $('.btn_hide2').on('click',function(){
+        $('.sitemap > li > ul > li > ul > li > ul').slideToggle(200);				
+        btn_hide2_click++;
+        if(btn_hide2_click % 2 == 0){
+            $(this).removeClass('hide');
+            $(this).html('3차메뉴 <i class="fas fa-eye">');
+        } else {
+            $(this).addClass('hide');
+            $(this).html('3차메뉴 <i class="fas fa-eye-slash">');
+        }
+    });
 });
