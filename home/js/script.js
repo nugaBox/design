727074cc06e40fdfcb69a7d5404d60bb436b0c77
NUGA <ngjang@kakao.com>
2021-05-08 23:00:25 +0900

    파일 위치 오류 수정

--- home/js/script.js
+++ home/js/script.js
@@ -11,53 +11,58 @@ $(document).ready(function() {
         e.preventDefault();
     });
 
-    // 작업 진행도 바
-    var taskNum = $('li').length;
-    var checkNum = $('.fa-check-circle').length;
-    var progress = checkNum / taskNum * 100
-    $('#progressNum').text(progress.toFixed(1)+'%');
-    $('#progressBar').progressbar({ value: progress });
-    
-    // 작업 진행상태 클릭
-    $('.progress').on('click',function(){
-        $(this).siblings('a').removeClass('active');
-        $(this).addClass('active');
-        
-        var progress = $(this).data('progress');
-        if( progress == 'done') {
-            $('.co-list > li').hide();
-            $('.co-list > li').has('.fa-check-circle').fadeIn();
-        } else if (progress == 'working') {
-            $('.co-list > li').hide();
-            $('.co-list > li').has('.fa-tools').fadeIn();
-        } else {
-            $('.co-list > li').fadeIn();
-        }
-    });
+    // 작업 진행내용 있는 경우
+    if($('.co-progress')){
+        // 작업 진행도 바
+        var taskNum = $('li').length;
+        var checkNum = $('.fa-check-circle').length;
+        var progress = checkNum / taskNum * 100
+        $('#progressNum').text(progress.toFixed(1)+'%');
+        $('#progressBar').progressbar({ value: progress });
+
+        // 작업 진행상태 클릭
+        $('.progress').on('click',function(){
+            $(this).siblings('a').removeClass('active');
+            $(this).addClass('active');
+            
+            var progress = $(this).data('progress');
+            if( progress == 'done') {
+                $('.co-list > li').hide();
+                $('.co-list > li').has('.fa-check-circle').fadeIn();
+            } else if (progress == 'working') {
+                $('.co-list > li').hide();
+                $('.co-list > li').has('.fa-tools').fadeIn();
+            } else {
+                $('.co-list > li').fadeIn();
+            }
+        });
+    }
 
     // 사이트맵 메뉴 숨김 버튼
-    var btn_hide1_click=0;
-    var btn_hide2_click=0;
-    $('.btn_hide1').on('click',function(){
-        $('.sitemap > li > ul > li > ul').slideToggle(200);		
-        btn_hide1_click++;
-        if(btn_hide1_click % 2 == 0){
-            $(this).removeClass('hide');
-            $(this).html('2차메뉴 <i class="fas fa-eye">');
-        } else {
-            $(this).addClass('hide');
-            $(this).html('2차메뉴 <i class="fas fa-eye-slash">');
-        }
-    });
-    $('.btn_hide2').on('click',function(){
-        $('.sitemap > li > ul > li > ul > li > ul').slideToggle(200);				
-        btn_hide2_click++;
-        if(btn_hide2_click % 2 == 0){
-            $(this).removeClass('hide');
-            $(this).html('3차메뉴 <i class="fas fa-eye">');
-        } else {
-            $(this).addClass('hide');
-            $(this).html('3차메뉴 <i class="fas fa-eye-slash">');
-        }
-    });
+    if($('.btn_hide1') || $('.btn_hide2')){
+        var btn_hide1_click=0;
+        var btn_hide2_click=0;
+        $('.btn_hide1').on('click',function(){
+            $('.sitemap > li > ul > li > ul').slideToggle(200);		
+            btn_hide1_click++;
+            if(btn_hide1_click % 2 == 0){
+                $(this).removeClass('hide');
+                $(this).html('2차메뉴 <i class="fas fa-eye">');
+            } else {
+                $(this).addClass('hide');
+                $(this).html('2차메뉴 <i class="fas fa-eye-slash">');
+            }
+        });
+        $('.btn_hide2').on('click',function(){
+            $('.sitemap > li > ul > li > ul > li > ul').slideToggle(200);				
+            btn_hide2_click++;
+            if(btn_hide2_click % 2 == 0){
+                $(this).removeClass('hide');
+                $(this).html('3차메뉴 <i class="fas fa-eye">');
+            } else {
+                $(this).addClass('hide');
+                $(this).html('3차메뉴 <i class="fas fa-eye-slash">');
+            }
+        });
+    }
 });
